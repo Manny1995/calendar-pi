2554a7390748aec68f37150660f08435d1e086dc
Manny1995 <immanuelda@gmail.com>
2018-04-24 20:19:02 -0700

    initial commit

---
+++ models.js
@@ -0,0 +1,92 @@
+var times = {
+	'Monday' : [
+		{
+			'start' : '11:00am',
+			'end' : '12:00am',
+			'description' : 'meeting',
+		},
+		{
+			'start' : '11:00am',
+			'end' : '12:00am',
+			'description' : 'meeting',
+		},
+		{
+			'start' : '11:00am',
+			'end' : '12:00am',
+			'description' : 'meeting',
+		},
+	],
+
+	'Tuesday' : [
+		{
+			'start' : '11:00am',
+			'end' : '12:00am',
+			'description' : 'meeting',
+		},
+		{
+			'start' : '11:00am',
+			'end' : '12:00am',
+			'description' : 'meeting',
+		},
+		{
+			'start' : '11:00am',
+			'end' : '12:00am',
+			'description' : 'meeting',
+		},
+	],
+
+	'Wednesday' : [
+		{
+			'start' : '11:00am',
+			'end' : '12:00am',
+			'description' : 'meeting',
+		},
+		{
+			'start' : '11:00am',
+			'end' : '12:00am',
+			'description' : 'meeting',
+		},
+		{
+			'start' : '11:00am',
+			'end' : '12:00am',
+			'description' : 'meeting',
+		}
+
+	]
+
+	'Thursday' : [
+		{
+			'start' : '11:00am',
+			'end' : '12:00am',
+			'description' : 'meeting',
+		},
+		{
+			'start' : '11:00am',
+			'end' : '12:00am',
+			'description' : 'meeting',
+		},
+		{
+			'start' : '11:00am',
+			'end' : '12:00am',
+			'description' : 'meeting',
+		},
+	],
+
+	'Friday' : [
+		{
+			'start' : '11:00am',
+			'end' : '12:00am',
+			'description' : 'meeting',
+		},
+		{
+			'start' : '11:00am',
+			'end' : '12:00am',
+			'description' : 'meeting',
+		},
+		{
+			'start' : '11:00am',
+			'end' : '12:00am',
+			'description' : 'meeting',
+		},
+	],
+}
---
+++ week.js
@@ -0,0 +1,20 @@
+
+
+var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+
+
+
+
+function createTable() {
+
+
+}
+
+
+function addBusyTimes() {
+
+
+}
+
+
+function 
